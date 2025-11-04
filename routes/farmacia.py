dbd94ea10e32279315e17e2ad085c45881012ac4
KARE <karelly355@gmail.com>
2025-11-04 01:29:10 -0500

    KARELLY FARMACIA

---
+++ routes/farmacia.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template, session, redirect, url_for
+
+farmacia_bp = Blueprint('farmacia', __name__, url_prefix='/farmacia')
+
+@farmacia_bp.before_request
+def check_session():
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    if session['usuario']['rol'] not in ['admin', 'farmacia']:
+        return redirect(url_for('unauthorized'))
+
+@farmacia_bp.route('/')
+def farmacia():
+    return render_template('panel.html', subsistema='farmacia')
+
+@farmacia_bp.route('/gestionar-recepcion-medicamentos')
+def gestionar_recepcion_medicamentos():
+    return render_template('gestionarRecepcionMedicamentos.html')
+
+@farmacia_bp.route('/gestionar-entrega-medicamentos')
+def gestionar_entrega_medicamentos():
+    return render_template('gestionarEntregaMedicamentos.html')
